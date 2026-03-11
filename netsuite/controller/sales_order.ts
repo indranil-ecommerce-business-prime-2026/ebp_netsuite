a8715657f1ec9d7e30477bf73fcddc898e7f9e93
Kim Tabilon <kim.gtabilon@gmail.com>
2026-03-11 18:42:15 +0800

    fix bugs

--- netsuite/controller/sales_order.ts
+++ netsuite/controller/sales_order.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from "express";
-import { getDb } from "../../master_list/config/mongdodb.config";
+import { getDb } from "../config/mongdodb.config";
 
 /* ============================
    Interfaces
